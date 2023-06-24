7994147924472e646a6bcd7e0656a0c00ec3793c
David Kosub <dave@kosub.us>
2023-06-23 20:13:29 -0500

    chore: basic instructor table

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,66 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { parseCsv } from "$lib/peloton/parser";
+  import { type Report, generateReport } from "$lib/peloton/report";
+
+  let report: Report;
+  let error: string;
+
+  const onChange = (event) => {
+    const files = event.target.files;
+    if (!files.length) {
+      error = "No files selected";
+      return;
+    }
+
+    if (files[0].type != "text/csv") {
+      error = "File provided is not a CSV";
+      return;
+    }
+
+    parseCsv(files[0], (result) => {
+      report = generateReport(result);
+    });
+  };
+</script>
+
+<svelte:head>
+  <title>Peloton Stats</title>
+</svelte:head>
+
+<h1>Peloton Stats</h1>
+
+{#if !report}
+  {#if error}
+    <div class="alert">
+      {error}
+    </div>
+  {/if}
+  <input type="file" name="file" accept="text/csv" on:change={onChange} />
+{:else}
+  <h2>Classes Taken With Instructor</h2>
+  <table>
+    <thead>
+      <tr>
+        <th>Instructor</th>
+        <th>Classes</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each [...report.instructors] as [instructor, classCount]}
+        <tr>
+          <td>{instructor}</td>
+          <td>{classCount}</td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+{/if}
+
+<style>
+  .alert {
+    margin: 1rem 0;
+    padding: 0.5rem 1rem;
+    background: darkred;
+    border-radius: 0.5rem;
+  }
+</style>
