3407a97f793fb8dc8f8e42377c7dabd40af7037e
David Kosub <dave@kosub.us>
2023-07-03 09:19:12 -0500

    chore: skip the last line

--- src/lib/peloton/parser.ts
+++ src/lib/peloton/parser.ts
@@ -5,7 +5,7 @@ export type ParsingComplete = (arg0: string[][]) => void;
 export const parseCsv = (file: File, onComplete: ParsingComplete) => {
   parse<string[]>(file, {
     complete(results) {
-      onComplete(results.data.slice(1));
+      onComplete(results.data.slice(1, -1));
     },
   });
 };
