23b811f57e4fcd0f49a14ae80aac8023fec19a27
David Kosub <dave@kosub.us>
2023-07-04 14:21:36 -0500

    chore: simplify

--- src/lib/peloton/instructors.ts
+++ src/lib/peloton/instructors.ts
@@ -3,8 +3,8 @@ export type Instructors = Map<String, number>;
 export const aggregate = (csvData: string[][]): Instructors => {
   return csvData.reduce((instructors, row) => {
     const instructor = row[2] || "No Instructor";
-    const value = instructors.get(instructor);
-    instructors.set(instructor, value ? value + 1 : 1);
+    const value = instructors.get(instructor) || 0;
+    instructors.set(instructor, value + 1);
     return instructors;
   }, new Map() as Instructors);
 };
