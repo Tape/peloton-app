6d7692fcd6c818c3d03eb29bae1f2fcbfe1f7ac7
David Kosub <dave@kosub.us>
2023-08-18 22:35:45 -0500

    chore: clean up math utils

--- src/lib/util/math.ts
+++ src/lib/util/math.ts
@@ -1,22 +1,13 @@
-export const sumOf = <T extends { [key: string]: any }, K extends keyof T>(
-  data: T[],
-  ...fields: K[]
-): { [key in K]: number } => {
-  // Initialize an object containing all K's provided with 0
-  const totals = fields.reduce(
-    (acc, field) => ({
-      ...acc,
-      [field]: 0,
-    }),
-    {} as { [key in K]: number },
+type TypedValues<T, V> = {
+  [K in keyof T]: T[K] extends V ? K : never;
+}[keyof T];
+
+export const sumOf = <T, K extends TypedValues<T, number>>(data: T[], ...fields: K[]): { [Key in K]: number } => {
+  return fields.reduce(
+    (acc, field) => {
+      acc[field] = data.reduce((sum: number, row: T) => sum + (row[field] as number), 0);
+      return acc;
+    },
+    {} as { [Key in K]: number },
   );
-
-  // Sum the total of all Ks for each row
-  data.forEach((row) => {
-    for (const field of fields) {
-      totals[field] += row[field];
-    }
-  });
-
-  return totals;
 };
